66166f6de98383c0bc45bceeff0a69714f1cf6dc
Brendon Gomes de Cordova <dev.gcbrendon@gmail.com>
2026-04-16 15:48:22 -0300

    feat(pdp): implement dynamic SKU selection and price reactivity
    - Optimize Product detail view with prefetch_related to avoid N+1 queries
    - Implement Vanilla JS logic to handle SKU selection without page reloads
    - Use data-attributes to pass backend pricing and IDs to the frontend safely
    - Dynamically update hidden form input to prepare for Cart integration

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from django.http import HttpResponse
 
-# Create your views here.
+def product_detail(request, slug):
+
+    product = get_object_or_404(Product.objects.prefetch_related('skus'), slug=slug)
+
+    available_skus = product.skus.filter(is_active=True)
+    default_sku = available_skus.first()
+
+    context = {
+        'product': product,
+        'skus': available_skus,
+        'default_sku': default_sku,
+    }
+    return render(request, 'products/product_detail.html', context)
