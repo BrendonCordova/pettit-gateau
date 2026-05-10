5492249e642a0ef55a8da3c93ba9dd4adb87fcec
Brendon Gomes de Cordova <dev.gcbrendon@gmail.com>
2026-05-10 16:37:34 -0300

    feat(checkout): implement address creation flow for new customers
    - Create AddressForm leveraging BaseModel architecture
    - Build address_create_view to handle user input and associate address with request.user
    - Design responsive address_form.html template with Bootstrap 5
    - Update checkout_view to dynamically redirect users without an address to the creation form before proceeding

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import CustomerLoginView, register_view
+from .views import CustomerLoginView, register_view, address_create_view
 
 app_name = 'customers'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', CustomerLoginView.as_view(), name='login'),
     path('cadastro/', register_view, name='register'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('endereco/novo/', address_create_view, name='adress-create'),
 ]
--- apps/customers/views.py
+++ apps/customers/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
-from .forms import CustomerCreationForm
+from .forms import CustomerCreationForm, AddressForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def address_create_view(request):
+    if request.method == 'POST':
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            address = form.save(commit=False)
+            address.customer = request.user
+            address.save()
+
+            return redirect('orders:checkout')
+    else:
+        form = AddressForm()
+
+    return render(request, 'customers/address_form.html', {'form': form})
 
 class CustomerLoginView(LoginView):
     template_name = 'customers/login.html'
@@ -13,7 +29,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('carts:cart-page')
+            return redirect('orders:checkout')
         else:
             print("\n--- ERROS DE VALIDAÇÃO DO FORMULÁRIO ---")
             print(form.errors)
